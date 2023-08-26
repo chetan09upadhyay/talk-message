89f2111ed2046747c7fa78afd940af0cc180c8f7
chetan09upadhyay <132812698+chetan09upadhyay@users.noreply.github.com>
2023-08-26 11:13:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors(
+     
+      origin: "https://chatoo.onrender.com",
+      credentials: true,
+    }",
+      credentials: true,
+    ));
 app.use(express.json());
 
 
