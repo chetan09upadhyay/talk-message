5cbe8d583341c4d5a964441d6619002c223d092b
chetan09upadhyay <132812698+chetan09upadhyay@users.noreply.github.com>
2023-08-26 09:46:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,14 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors(
+     
+    origin: "talk-message-f8wk7ul22-chetan09upadhyay.vercel.app",
+    methods: ["POST","GET"],
+    credentials: true,
+ 
+    
+));
 app.use(express.json());
 
 
